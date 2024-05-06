af5a0ae666496cfba069a02278c143385c8647a4
quityQ <63550897+quityQ@users.noreply.github.com>
2024-05-06 22:31:28 +0200

    name generalized

--- src/main/java/ch/zhaw/deeplearningjava/sports/ImageApplication.java
+++ src/main/java/ch/zhaw/deeplearningjava/sports/ImageApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FootwearApplication {
+public class ImageApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FootwearApplication.class, args);
+		SpringApplication.run(ImageApplication.class, args);
 	}
 
 }
